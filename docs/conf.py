723158b5f364596def7c78b231b8640d23bc7313
Matt Limb <matt.limb17@gmail.com>
2022-07-24 14:20:27 +0100

    Removed CLI package click and removed tests pertaining to click.

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,6 @@ copyright = "2022, Matt Limb"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_click",
     "myst_parser",
 ]
 autodoc_typehints = "description"
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,6 @@
-"""Test cases for the __main__ module."""
-import pytest
-from click.testing import CliRunner
-
-from pylogformats import __main__
+"""Demo Test Cases."""
 
 
-@pytest.fixture
-def runner() -> CliRunner:
-    """Fixture for invoking command-line interfaces."""
-    return CliRunner()
-
-
-def test_main_succeeds(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
-    assert result.exit_code == 0
+def test_main_succeeds() -> None:
+    """Demo Test which returns True."""
+    assert True
