c718354461759c7c855e3e243a9293e9f9c282b7
Matt Limb <matt.limb17@gmail.com>
2022-07-28 11:51:47 +0100

    Setting a Flake8 ignore due to subclassing a pre-existing non-pep8 compliant class

--- src/pylogformats/json/bunyan.py
+++ src/pylogformats/json/bunyan.py
@@ -54,7 +54,10 @@ With Extra", "hostname": ..., "v": 0, "whatami": "An Extra"}
 
     """
 
-    def formatTime(
+    # Following line ignores Flake8 because this overrides a pre-existing
+    # function which is formatted using CamelCase instead of the PEP8 approved
+    # snake case. This allows it to comply with the logging.LogFormatter subclass. 
+    def formatTime(  # noqa: N802
         self, record: logging.LogRecord, datefmt: Optional[str] = None
     ) -> str:
         """Format LogRecord Times into the correct formatted string String.
